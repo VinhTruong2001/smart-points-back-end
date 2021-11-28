7cf9502fa2958eb90760b6bbf4fb6fb323d3890c
VinhTruong2001 <ducvinh.truong2001@gmail.com>
2021-11-28 23:54:19 +0700

    Change Setting

--- smartpoint/settings.py
+++ smartpoint/settings.py
@@ -112,7 +112,7 @@ DATABASES['default'].update(db_from_env)
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'media'),
 )
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
