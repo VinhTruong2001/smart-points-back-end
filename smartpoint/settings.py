76199b6ce7aa42e8e014cd42801ca07155dcd207
VinhTruong2001 <ducvinh.truong2001@gmail.com>
2021-11-29 10:19:25 +0700

    Change setting

--- smartpoint/settings.py
+++ smartpoint/settings.py
@@ -110,7 +110,7 @@ DATABASES = {
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'media'),
+    os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
